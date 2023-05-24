8b0173b1876dd00b12522e460f8e3d351f402d4d
Malgorzata Kacperczyk <g.ochaaa@gmail.com>
2023-05-24 16:04:40 +0200

    add 1/1 task

---
+++ js/task-01.js
@@ -0,0 +1,4 @@
+const categoriesList = document.querySelector("#categories");
+const ItemsList = categoriesList.querySelectorAll("li.item");
+const categoriesCount = ItemsList.length;
+console.log("Number of categories:" + categoriesCount);
