4fbc9bb193b7a88f2c2a6b007f002cfea39b72f2
Malgorzata Kacperczyk <g.ochaaa@gmail.com>
2023-05-25 21:12:45 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach(function (ingredient) {
+  const itemList = document.createElement("li");
+  itemList.textContent = ingredient;
+  itemList.classList.add("item");
+  ingredientsList.appendChild(itemList);
+  console.log(itemList);
+});
