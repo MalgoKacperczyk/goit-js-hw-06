2f25f6bc12170a1b70b02d8d69dc1ef09b618b7e
Malgorzata Kacperczyk <g.ochaaa@gmail.com>
2023-05-25 15:41:47 +0200

    add task 1/2

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,14 @@
 const categoriesList = document.querySelector("#categories");
-const ItemsList = categoriesList.querySelectorAll("li.item");
-const categoriesCount = ItemsList.length;
+const categoryItem = categoriesList.querySelectorAll("li.item");
+const categoriesCount = categoryItem.length;
+
 console.log("Number of categories:" + categoriesCount);
+
+const itemName = categoryItem.forEach(function (item) {
+  const categoryName = item.querySelector("h2").textContent;
+  const element = item.querySelectorAll("li");
+  const elementCount = element.length;
+
+  console.log("Categories:" + categoryName);
+  console.log("Elements:" + elementCount);
+});
