208541eb9d2b67da6dce3eda4d47c4301b46ddfd
Malgorzata Kacperczyk <g.ochaaa@gmail.com>
2023-05-26 23:02:02 +0200

    add task 4

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const buttonDecrement = document.querySelector("[data-action='decrement']");
+const buttonIncrement = document.querySelector("[data-action='increment']");
+const value = document.getElementById("value");
+let counterValue = 0;
+
+function changeCounter() {
+  value.textContent = counterValue;
+}
+buttonIncrement.addEventListener("click", function () {
+  counterValue += 1;
+  changeCounter();
+});
+
+buttonDecrement.addEventListener("click", function () {
+  counterValue--;
+  changeCounter();
+});
+
+changeCounter();
