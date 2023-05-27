0411ae537145c2e58655dcc8782f3953376ebda2
Malgorzata Kacperczyk <g.ochaaa@gmail.com>
2023-05-27 21:06:49 +0200

    add task 6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const validationInput = document.getElementById("validation-input");
+const expectedLength = parseInt(validationInput.getAttribute("data-length"));
+
+function checkLength() {
+  const inputLength = validationInput.value;
+
+  if (inputLength.length === expectedLength) {
+    validationInput.classList.remove("invalid");
+    validationInput.classList.add("valid");
+  } else {
+    validationInput.classList.remove("valid");
+    validationInput.classList.add("invalid");
+  }
+}
+validationInput.addEventListener("blur", checkLength);
