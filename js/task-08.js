b16c6ab6238d6f146d30e7d990648fc812d6ca8f
Malgorzata Kacperczyk <g.ochaaa@gmail.com>
2023-05-29 18:40:29 +0200

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const loginForm = document.querySelector(".login-form");
+console.log(loginForm);
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (email.value === "" || password.value === "") {
+    return console.log("Wypełnij wszystkie pola");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
