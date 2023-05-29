2e783ed307782bee142afb504a114455c9b56cd1
Malgorzata Kacperczyk <g.ochaaa@gmail.com>
2023-05-29 19:19:44 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const widget = document.querySelector(".widget");
+console.log(widget);
+
+const backgroundColor = document.querySelector(".color");
+console.log(backgroundColor);
+
+const button = document.querySelector(".change-color");
+console.log(button);
+
+changeBackgroundColor();
+
+widget.addEventListener("change", changeBackgroundColor);
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  backgroundColor.style.backgroundColor = randomColor;
+  backgroundColor.textContent = randomColor;
+}
